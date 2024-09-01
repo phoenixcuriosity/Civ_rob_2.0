e4dd04f1d09e0a9f15c82f1e3406d99bb3337145
Robin SAUTER <robin.sauter@orange.fr>
2024-09-01 12:15:13 +0200

    A_0012_001: Add an abstract Screen class replacing a struct
    This change allow to reuse some of the code, instead of duplicating it.
    
    1 GUI
    2 Camera : HUD and MAP
    2 SpritBatch : Static and Dynamic
    1 AudioEngine

---
+++ R2D/src/Screen.h
@@ -0,0 +1,72 @@
+/*
+
+	Civ_rob_2
+	Copyright SAUTER Robin 2017-2024 (robin.sauter@orange.fr)
+
+	You can check for update on github.com -> https://github.com/phoenixcuriosity/Civ_rob_2.0
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#include "AudioEngine.h"
+#include "Camera2D.h"
+#include "GUI.h"
+#include "SpriteBatch.h"
+
+namespace R2D
+{
+
+class CScreen
+{
+public:
+	CScreen();
+	~CScreen();
+
+public:
+
+	bool init(const int width, const int height);
+	bool end();
+
+public:
+
+	void initUI();
+	void initHUDText();
+	void drawAll();
+	void updateInputManager(SDL_Event& ev, InputManager& resourceManager);
+
+private:
+
+	virtual void doInitUI() = 0;
+	virtual void doInitHUDText() = 0;
+	virtual void doDrawAll() = 0;
+
+protected:
+	Camera2D m_camera;
+	Camera2D m_cameraHUD;
+
+	SpriteBatch m_spriteBatchHUDDynamic;
+	SpriteBatch m_spriteBatchHUDStatic;
+
+	AudioEngine m_audioEngine;
+
+	GUI m_gui;
+};
+
+}
+
+#endif /* SCREEN_H */
