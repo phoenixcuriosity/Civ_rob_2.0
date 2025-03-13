baf1a991b9e1cb4176ce1fcd36e60fa1e8c663c6
Robin SAUTER <robin.sauter@orange.fr>
2025-03-14 00:30:05 +0100

    A_0024_004: Refactor FileWriter and FileReader
    Use template, see if really needed

---
+++ Civ_Rob_2.0/src/FileReader.cpp
@@ -0,0 +1,63 @@
+/*
+
+	Civ_rob_2
+	Copyright SAUTER Robin 2017-2025 (robin.sauter@orange.fr)
+
+	You can check for update on github.com -> https://github.com/phoenixcuriosity/Civ_rob_2.0
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#pragma once
+
+#include "FileReader.h"
+
+FileReader
+::FileReader(const FilePath& filename)
+: m_file(filename, std::ios::in)
+{
+    if (!m_file)
+    {
+        throw std::runtime_error("Unable to open file: " + filename);
+    }
+}
+
+FileReader::Data
+FileReader
+::read(const FilePath& filename)
+{
+    std::error_code ec;
+    const uintmax_t length = std::filesystem::file_size(filename, ec);
+    if (ec)
+    {
+        throw std::runtime_error(ec.message());
+    }
+    Data buffer(length, '\0');
+    m_file.read(buffer.data(), length);
+
+    const Data text{ std::string(buffer.c_str(), strlen(buffer.c_str())) };
+    if (text.empty())
+    {
+        throw std::runtime_error("Empty file");
+    }
+
+    return text;
+}
+
+jsoncons::ojson
+FileReader
+::readJson(const FilePath& filename)
+{
+    return jsoncons::ojson::parse(read(filename));
+};
---
+++ Civ_Rob_2.0/src/FileWriter.h
@@ -0,0 +1,67 @@
+/*
+
+	Civ_rob_2
+	Copyright SAUTER Robin 2017-2025 (robin.sauter@orange.fr)
+
+	You can check for update on github.com -> https://github.com/phoenixcuriosity/Civ_rob_2.0
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <jsoncons/json.hpp>
+
+
+class FileWriter
+{
+public:
+    FileWriter(const std::string& filename)
+    : m_file(filename, std::ios::out | std::ios::binary)
+    {
+        if (!m_file)
+        {
+            throw std::runtime_error("Unable to open file: " + filename);
+        }
+    }
+
+    template <typename T>
+    void write(const T& data)
+    {
+        if constexpr (std::is_same_v<T, std::string>)
+        {
+            m_file.write(data.data(), data.size());
+        }
+        else if constexpr (std::is_same_v<T, jsoncons::ojson>)
+        {
+            jsoncons::encode_json(data, m_file, jsoncons::indenting::indent);
+        }
+        else
+        {
+            static_assert(sizeof(T) == 0, "Unsupported type for FileWriter.");
+        }
+
+        if (!m_file)
+        {
+            throw std::runtime_error("Error writing to file.");
+        }
+    }
+
+    ~FileWriter() = default;
+
+private:
+    std::ofstream m_file;
+};
