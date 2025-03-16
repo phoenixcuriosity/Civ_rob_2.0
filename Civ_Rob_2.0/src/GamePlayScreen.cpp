60a647f373ef2aff0408861ac08b25c0ef11124a
Robin SAUTER <robin.sauter@orange.fr>
2025-03-16 17:48:15 +0100

    A_0024_015: Fix GamePlayScreen load Subscriber

--- Civ_Rob_2.0/src/GamePlayScreen.cpp
+++ Civ_Rob_2.0/src/GamePlayScreen.cpp
@@ -54,7 +54,7 @@ m_userInputNewGame(userInputNewGame)
 
 R2D::RegisterPairVector GamePlayScreen::addSubscriber()
 {
-	R2D::RegisterPairVector registerLoad{ };
+	R2D::RegisterPairVector registerLoad{ {this, typeid(MainMapConfig)}, {this, typeid(UnitTemplate)}, {this, typeid(CityNameTemplate)} };
 	return registerLoad;
 }
 
