44820e5e1da0af7a36cdb8251943375a6b474718
Robin SAUTER <robin.sauter@orange.fr>
2025-07-23 21:44:52 +0200

    A_0032_001: Add IBuild interface class

---
+++ Civ_Rob_2.0/src/City/IBuild.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <memory>
+
+#include <jsoncons/json.hpp>
+
+class Player;
+
+namespace R2D
+{
+	class Coor;
+}
+
+namespace city
+{
+
+class IBuild
+{
+public:
+	using computeReturnedValue = std::pair<bool, double>;
+
+protected:
+	using PlayerPtrT = std::shared_ptr<Player>;
+
+public:
+	virtual ~IBuild() = default;
+
+	virtual computeReturnedValue computeWorkToBuild(const double work, PlayerPtrT& player, const R2D::Coor& coor) = 0;
+
+	virtual void save(jsoncons::ojson& saveTo) = 0;
+
+	virtual double getRemainingWorkoverWork() const = 0;
+};
+
+}
